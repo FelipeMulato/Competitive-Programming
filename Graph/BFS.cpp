92cf8c8d559c1e509f1b66dfc7b19db7ebccf650
FelipeMulato <felipe.a.mulato@gmail.com>
2026-01-20 16:44:46 -0300

    Adding graph bfs

---
+++ Graph/BFS.cpp
@@ -0,0 +1,27 @@
+using namespace std;
+#define endl '\n'
+#define ll long long
+vector<vector<ll>> v;
+vector<bool> vis;
+void DFS(int u){
+    int n = v.size();
+    vector<int> dist(n+1,-1);
+    queue<int> q;
+    dist[u]=0;
+    q.push(u);
+    while (!q.empty()){
+        int top = q.front(); q.pop();
+        for(auto w:v[top]){
+            if(dist[w]==-1){
+                dist[w] = 1+dist[top];
+                q.push(w);
+            }
+        }
+    
+    }
+}
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    
+}
