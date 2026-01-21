469f0201a0c4d40d49ce0b40e7d8958d748a7a45
FelipeMulato <felipe.a.mulato@gmail.com>
2026-01-21 11:54:02 -0300

    Adding DFS to see if a graph is bipartite

--- Graph/DFS.cpp
+++ Graph/DFS.cpp
@@ -11,6 +11,17 @@ void DFS(int u){
     }
     return;
 }
+//Test if a given graph is bipartite
+bool DFS_Color(int u, vector<int> &color, int c){
+    color[u] = c;
+    for(auto next:v[u]){
+        if(color[u]==0){
+            if(!DFS_Color(next,color,c*(-1))) return false;
+        }
+        if(color[u]==color[next]) return false;
+    }
+    return true;
+}
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
