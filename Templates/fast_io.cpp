2ca82a2d091832874745a279cec178cc396eb33f
FelipeMulato <felipe.a.mulato@gmail.com>
2026-01-26 17:35:59 -0300

    Uptade on basic template

--- Templates/fast_io.cpp
+++ Templates/fast_io.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 #define endl '\n'
 #define ll long long
-
+#define ld long double
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    cout<<"Hello World"<<endl;
+    int tc; cin>>tc; while (tc--){
+        // your code here
+    }
+    
 }
