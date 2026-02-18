3b349adf4be8671b05c8a4f000416e7b8089689c
FelipeMulato <felipe.a.mulato@gmail.com>
2026-02-18 10:11:47 -0300

    Adding Modular sum

---
+++ Number Theory/modular.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define ld long double
+ll const MOD = 1e9+7;
+ll mod_add(ll a, ll b) return (a%MOD + b%MOD)%MOD;
