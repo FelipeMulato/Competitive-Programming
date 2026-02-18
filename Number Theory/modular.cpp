3b23284fb02c779a7e6fc0ed78579c86c76309e2
FelipeMulato <felipe.a.mulato@gmail.com>
2026-02-18 14:25:49 -0300

    Fixing Syntax

--- Number Theory/modular.cpp
+++ Number Theory/modular.cpp
@@ -4,9 +4,9 @@ using namespace std;
 #define ll long long
 #define ld long double
 ll const MOD = 1e9+7;
-ll mod_add(ll a, ll b) return (a%MOD + b%MOD)%MOD;
-ll mod_mut(ll a, ll b) return (a%MOD * b%MOD)%MOD;
-ll mod_sub(ll a, ll b) return (a%MOD - b%MOD + MOD)%MOD;
+ll mod_add(ll a, ll b){ return (a%MOD + b%MOD)%MOD;}
+ll mod_mut(ll a, ll b){ return (a%MOD * b%MOD)%MOD;}
+ll mod_sub(ll a, ll b){ return (a%MOD - b%MOD + MOD)%MOD;}
 
 int main(){
 
