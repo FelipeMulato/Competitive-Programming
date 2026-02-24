d2eeb33f4fcdb7d2709b075502a2e9d7331b5472
FelipeMulato <felipe.a.mulato@gmail.com>
2026-02-24 11:28:02 -0300

    Fast way to calculate power

---
+++ Number Theory/binarypower.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define ld long double
+
+ll fastpower(ll a,ll b){
+  ll res=1;
+  while(b>0){
+    if(b%2==0) res*=a;
+    a*=a;
+    b/=2;
+  }
+  return res;
+}
+int main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  int tc;tc=1;while(tc--){
+        cout<<fastpower(5,5)<<endl;
+    }
+}
