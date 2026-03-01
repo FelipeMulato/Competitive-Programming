b75fb58636a8c368b205b9db875b59e98963dffd
FelipeMulato <felipe.a.mulato@gmail.com>
2026-03-01 17:14:33 -0300

    FEAT: adding basic mod and direction array

--- Templates/fast_io.cpp
+++ Templates/fast_io.cpp
@@ -3,6 +3,9 @@ using namespace std;
 #define endl '\n'
 #define ll long long
 #define ld long double
+const ll mod = 1e9+7;
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
