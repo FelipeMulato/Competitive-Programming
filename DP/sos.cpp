a8e7cc86325f7cd640f040e13b217aa52600c3b2
FelipeMulato <felipe.a.mulato@gmail.com>
2026-06-22 14:50:12 -0300

    FEAT: adding better descreption of the dp

--- DP/sos.cpp
+++ DP/sos.cpp
@@ -8,6 +8,7 @@ int dx[] = {0, 0, 1, -1};
 int dy[] = {1, -1, 0, 0};
 ll const inf  = 1e9+7;
 /// SOS DP: Sum over subsets DP
+// Computes f[mask] = sum of g[submask] for all submasks of mask(submask & mask == submask)
 void SOS(vector<ll> &f, int n){
     for(int i=0;i<n;i++){
         for(int mask=0;mask<(1<<n);mask++){
