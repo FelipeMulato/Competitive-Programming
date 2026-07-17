5f7f0acde130310b0eeeb54ac9030e6851c4ee5f
FelipeMulato <felipe.a.mulato@gmail.com>
2026-07-17 17:28:05 -0300

    FEAT: adding new implemenatation

--- Data Structed/segtree.cpp
+++ Data Structed/segtree.cpp
@@ -1,88 +1,83 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define endl '\n'
-#define ll long long
-#define ld long double
-const ll mod = 1e9+7;
-struct Node{
-  ll num;
-  Node(){
-    num = 0;
-  }
+struct Node {
+    ll val;
+   
+    Node() : val(0); {}
+    Node(ll v) : val(v) {}
 };
-Node Merge(Node a,Node b){
-  Node out;
-  // add problem logic here
-  out.num = a.num + b.num;
-  return out;
-}
-struct SegTree{
-  int sz;
-  vector<Node> seg;
-  SegTree(int n){
-    sz = 1;
-    while(sz<=n) sz*=2;
-    seg = vector<Node>(sz*2);
-  }
-  void build(const vector<ll>& a, ll x, ll lx, ll rx){
-        if(rx-lx==1){
-            if(lx< a.size()){
-                seg[x].num = a[lx];
-            }
+
+Node merge(Node vl, Node vr) { 
+    //Add logic here
+ }
+
+struct SegTree {
+    int N;
+    vector<Node> seg;
+
+    SegTree(int n) : N(n), seg(4 * n) {}
+
+    SegTree(vector<ll> &v) : N(v.size()), seg(4 * v.size()) {
+        build(v, 1, 0, N - 1);
+    }
+
+    void build(vector<ll> &v, int p, int l, int r) {
+        if (l == r) {
+            seg[p] = Node(v[l]);
+            return;
+        }
+
+        int m = (l + r) / 2;
+
+        build(v, 2 * p, l, m);
+        build(v, 2 * p + 1, m + 1, r);
+        
+        seg[p] = merge(seg[2 * p], seg[2 * p + 1]);
+    }
+
+    Node query(int a, int b, int p, int l, int r) {
+        if (b < l or r < a) return Node();
+        if (a <= l and r <= b) return seg[p];
+
+        int m = (l + r) / 2;
+
+        return merge(query(a, b, 2 * p, l, m), 
+                    query(a, b, 2 * p + 1, m + 1, r));
+    }
+
+    
+    void update1(int idx, ll val, int p, int l, int r) {
+        if (idx < l or r < idx) return;
+        if (l == r) {
+            seg[p].val=val;
             return;
         }
         
-        int mid = (rx+lx)/ 2;
+        int m = (l + r) / 2;
         
-        build(a, 2*x + 1, lx, mid);
-        build(a, 2*x + 2, mid, rx);
+        update1(idx, val, 2 * p, l, m);
+        update1(idx, val, 2 * p + 1, m + 1, r);
         
-        seg[x] = Merge(seg[2*x + 1], seg[2*x + 2]);
+        seg[p] = merge(seg[2 * p], seg[2 * p + 1]);
     }
-  void update(ll i, ll v, ll x,ll lx,ll rx){
-    if(rx-lx==1){
-      seg[x].num+=v;
-      return;
-    }
-    int mid = (rx+lx)/2;
-    if(i<mid) update(i,v,2*x+1,lx,mid);
-    else update(i,v,2*x+2,mid,rx);
-    
-    seg[x] = Merge(seg[2*x+1],seg[2*x+2]);
-  }
-  Node query(ll l, ll r, ll x, ll lx,ll rx){
-    if(lx>=r||rx<=l){
-      Node out; return out;
-    }
-    if(lx>=l && r>=rx) return seg[x];
-    int mid = (rx+lx)/2;
-    
-    return Merge(query(l,r,2*x+1,lx,mid),query(l,r,2*x+2,mid,rx));
-  }
-  void update2(ll l, ll r, ll x, ll lx, ll rx) {
-        if (lx >= r || rx <= l) return;
-        if (rx - lx == 1) {
-            //Update the leaf node here
+    void update2(int a, int b, ll val, int p, int l, int r) {
+      
+        if (b < l or r < a) return;
+        
+        if (l == r) {
+            seg[p].val=val;
             return;
         }
         
-        int mid = (rx + lx) / 2;
-        update2(l, r, 2 * x + 1, lx, mid);
-        update2(l, r, 2 * x + 2, mid, rx);
+        int m = (l + r) / 2;
         
-        seg[x] = Merge(seg[2 * x + 1], seg[2 * x + 2]);
+        
+        update2(a, b, val, 2 * p, l, m);
+        update2(a, b, val, 2 * p + 1, m + 1, r);
+        
+        seg[p] = merge(seg[2 * p], seg[2 * p + 1]);
     }
+
+    
+    ll query(int a, int b) { return query(a, b, 1, 0, N - 1).val1; }
+    void update1(int idx, ll val) { update1(idx, val, 1, 0, N - 1); }
+    void update2(int l,int r, ll val) { update2(l,r, val, 1, 0, N - 1); }
 };
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    
-    }
-    
-    
-    
-    
-    
-    
-    
-}
