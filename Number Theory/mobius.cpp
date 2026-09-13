78348992edaf8ed9ff658ec9ac33ca49c8318834
FelipeMulato <felipe.a.mulato@gmail.com>
2026-09-13 20:22:52 -0300

    FEAT: addingg mobius sieve

--- Number Theory/mobius.cpp
+++ Number Theory/mobius.cpp
@@ -6,8 +6,8 @@ bool comp[MAXN];
 vector<int> primes;
 
 void mobius() {
-    memeset(comp, 0, sizeof(comp));
-    memeset(mu, 0, sizeof(mu));
+    memset(comp, 0, sizeof(comp));
+    memset(mu, 0, sizeof(mu));
     mu[1] = 1;
     for (int i = 2; i < MAXN; ++i) {
         if (!comp[i]) primes.push_back(i), mu[i] = -1;
